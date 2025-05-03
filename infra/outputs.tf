84eedd7c78996523d22009bca9139306d84ce3d3
Parth-Patel007 <ppate220@asu.edu>
2025-05-03 11:41:42 -0700

    add kraft, producer complete

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,23 +1,29 @@
-output "kafka_bootstrap_server" {
-  value = "localhost:9092"
+output "kafka_bootstrap_servers" {
+  description = "Bootstrap address for Kafka (host‐accessible)"
+  value       = "localhost:9092"
 }
 
 output "schema_registry_url" {
-  value = "http://localhost:8081"
+  description = "URL for Schema Registry"
+  value       = "http://localhost:8081"
 }
 
-output "redis_address" {
-  value = "localhost:6379"
+output "redis_url" {
+  description = "URL for Redis"
+  value       = "localhost:6379"
 }
 
-output "postgres_dsn" {
-  value = "postgres://admin:admin@localhost:5432/payshield_audit?sslmode=disable"
+output "postgres_url" {
+  description = "JDBC URL for Postgres"
+  value       = "jdbc:postgresql://localhost:5432/payshield_audit"
 }
 
 output "prometheus_url" {
-  value = "http://localhost:9090"
+  description = "Prometheus UI URL"
+  value       = "http://localhost:9090"
 }
 
 output "grafana_url" {
-  value = "http://localhost:3000"
+  description = "Grafana UI URL"
+  value       = "http://localhost:3000"
 }
--- infra/variables.tf
+++ infra/variables.tf
@@ -1,28 +1,33 @@
 variable "docker_network_name" {
-  type    = string
-  default = "payshield_net"
+  default = "payshield"
 }
+
 
 variable "kafka_image" {
   type    = string
   default = "confluentinc/cp-kafka:7.6.0"
 }
+
 variable "schema_registry_image" {
   type    = string
   default = "confluentinc/cp-schema-registry:7.6.0"
 }
+
 variable "redis_image" {
   type    = string
   default = "redis:7.0-alpine"
 }
+
 variable "postgres_image" {
   type    = string
   default = "postgres:15"
 }
+
 variable "prometheus_image" {
   type    = string
   default = "prom/prometheus:latest"
 }
+
 variable "grafana_image" {
   type    = string
   default = "grafana/grafana:latest"
